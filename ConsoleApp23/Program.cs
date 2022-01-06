a8175a2b2e38e02776d5292023ddabb12e1fe350
4R4S8 <arashzomorrodi24@gmail.com>
2022-01-06 22:04:25 +0330

    Add project files.

---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,24 @@
+﻿int x = 0;
+int y = 0;
+int e = 0;
+Console.Write("Enter Your Goal Number: ");
+int z = int.Parse(Console.ReadLine());
+Console.Write("Enter The Limit of Numbers: ");
+int w = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < w; i++)
+{
+    x++;
+    for (int j = 0; j <= w; j++)
+    {
+        e = x * y;
+        if (e == z)
+        {
+            Console.WriteLine("{0} * {1} = {2}", x, y, e);
+        }
+        y++;
+    }
+    y = 0;
+}
+
+
